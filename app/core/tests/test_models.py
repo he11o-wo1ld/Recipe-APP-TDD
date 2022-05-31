e6bbaf489f19ac238da7cf6131b2d7cc700b607d
krishdp <95845806+krishdp@users.noreply.github.com>
2022-05-31 10:23:02 +0530

    Test Tag API.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,6 +10,11 @@ from django.contrib.auth import get_user_model
 from .. import models
 
 
+def create_user(email='user@example.com', password='password1234'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -68,3 +73,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test Creating a Tag Successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,12 +3,12 @@ Views for the recipe APIs.
 """
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
-from recipe import serializers
+from core.models import Recipe, Tag
+from . import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -32,3 +32,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
